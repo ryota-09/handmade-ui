f7fec14ef891d22d3a7f0402da1dfd6c158fddf4
ryota-09 <n.ryo950911@gmail.com>
2022-09-10 15:43:08 +0900

    colorsの設定

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,8 @@
+import { addDecorator } from '@storybook/react'
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { theme } from '../src/themes'
+import * as NextImage from 'next/image'
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
---
+++ src/themes/colors.ts
@@ -0,0 +1,17 @@
+const colors = {
+  primary: '#009432',
+  primaryDark: '#006266',
+  primaryLight: '#A3CB38',
+  secondary: '#747d8c',
+  secondaryDark: '#57606f',
+  secondaryLight: '#ced6e0',
+  border: '#cdced2',
+  danger: '#ed1c24',
+  dangerDark: '#a50d12',
+  gray: '#6b6b6b',
+  black: '#000000',
+  white: '#ffffff',
+  text: '#000000',
+} as const
+
+export default colors
