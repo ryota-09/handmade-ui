7b21f454d3c684782610db02851898cd53dcb74a
ryota-09 <n.ryo950911@gmail.com>
2022-10-01 15:31:36 +0900

    flexコンポーネントに利用するデータ型の作成

--- src/types/styles.d.ts
+++ src/types/styles.d.ts
@@ -8,36 +8,61 @@ export type ResponsiveProp<T> = {
 
 export type Responsive<T> = T | ResponsiveProp<T>
 
-const keys = [ "name", "test" ] as const 
+/**
+ * Flex
+ */ 
+type SelfPosition = "center" | "end" | "flex-end" | "flex-start" | "self-end" | "self-start" | "start"
 
-type Test = {
-  [key in typeof keys[number]]: number
-}
+type ContentPosition = "center" | "end" | "flex-end" | "flex-start" | "start"
 
-type Test<T> = T extends string ? string : number
-type Test = <T extends string | number>(name: T) => T extends string ? boolean : number
-type Test<T> = T extends Array< infer X> ? X : T
+type ContentDistoribution = "space-around" | "space-between" | "space-evenly" | "stretch"
 
-type Test1 = {
-  name: "taro",
-  value: number
-}
-type Test2 = {
-  name: "shiro",
-  value: string
-}
-type TestX = Test1 | Test2
-type TestKey = TestX["name"]
+type CSSPropatyGlobals = "-moz-initial" | "inherit" | "initial" | "revert" | "unset"
 
-type Test<X> = <T extends TestKey>(name: T) => X extends { name: T, value: infer U } ? U : never
+export type CSSPropatyAlignItems = CSSPropatyGlobals | SelfPosition | "baseline" | "stretch"
 
-type MyPick<Type, Keys extends keyof Type> = {
-  [key in Keys]: Type[Keys]
-}
+export type CSSPropertyAlignContent = CSSPropatyGlobals | ContentDistoribution | "center" | "end" | "flex-end" | "flex-start" | "start" | "baseline" | "normal"
 
-type Join<T, U > = T extends [infer A, ...infer B] ? `${A}${U}${Join<B, U>}`: T
-const T = Join<["foo", "bar"], "1">
+export type CSSPropertyJustifyContent = CSSPropatyGlobals | ContentDistoribution | ContentPosition | "left" | "normal" | "right" 
 
-type StrNum = string | number
-type Join<T, U extends StrNum> = T extends [infer A extends StrNum, ...infer B extends StrNum[]] ? `${A}${U}${Join<B, U>}`: T
-const S = Join<["foo", "bar"], ":">
+export type CSSPropertyFlexWrap = CSSPropatyGlobals | "nowrap" | "wrap" | "wrap-reverse"
+
+export type CSSPropertyFlexDirection = CSSPropatyGlobals | "column" | "column-reverse" | "row" | "row-reverse"
+
+export type CSSPropertyJustifySelf = CSSPropatyGlobals | SelfPosition | "auto" | "baseline" | "left" | "normal" | "right" | "stretch"
+
+export type CSSPropertyAlignSelf = CSSPropatyGlobals | SelfPosition | "auto" | "baseline" | "normal" | "stretch"
+
+// const keys = [ "name", "test" ] as const 
+
+// type Test = {
+//   [key in typeof keys[number]]: number
+// }
+
+// type Test<T> = T extends string ? string : number
+// type Test = <T extends string | number>(name: T) => T extends string ? boolean : number
+// type Test<T> = T extends Array< infer X> ? X : T
+
+// type Test1 = {
+//   name: "taro",
+//   value: number
+// }
+// type Test2 = {
+//   name: "shiro",
+//   value: string
+// }
+// type TestX = Test1 | Test2
+// type TestKey = TestX["name"]
+
+// type Test<X> = <T extends TestKey>(name: T) => X extends { name: T, value: infer U } ? U : never
+
+// type MyPick<Type, Keys extends keyof Type> = {
+//   [key in Keys]: Type[Keys]
+// }
+
+// type Join<T, U > = T extends [infer A, ...infer B] ? `${A}${U}${Join<B, U>}`: T
+// const T = Join<["foo", "bar"], "1">
+
+// type StrNum = string | number
+// type Join<T, U extends StrNum> = T extends [infer A extends StrNum, ...infer B extends StrNum[]] ? `${A}${U}${Join<B, U>}`: T
+// const S = Join<["foo", "bar"], ":">
